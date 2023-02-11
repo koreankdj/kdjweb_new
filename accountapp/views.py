35c9ee2345e7e95f0d61e7cbd24da85e9e53c188
koreankdj <koreankdj@gmail.com>
2023-02-12 00:56:24 +0900

    django course 22 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 from accountapp.models import HelloWorld
@@ -16,8 +19,14 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'text': hello_world_list})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # class형 view에서는 reverse_lazy를 사용해야 함
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
